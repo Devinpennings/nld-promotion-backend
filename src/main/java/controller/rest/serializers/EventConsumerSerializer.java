030a916b473493dd9fe722f7ccc63bd0644fb131
Devin Pennings <d.pennings@student.fontys.nl>
2019-06-12 18:19:17 +0200

    Fix

--- src/main/java/controller/rest/serializers/EventConsumerSerializer.java
+++ src/main/java/controller/rest/serializers/EventConsumerSerializer.java
@@ -1,4 +1,21 @@
 package controller.rest.serializers;
 
-public class EventConsumerSerializer {
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import events.EventConsumer;
+
+import java.io.IOException;
+
+public class EventConsumerSerializer extends JsonSerializer<EventConsumer> {
+
+    @Override
+    public void serialize(EventConsumer aClass, JsonGenerator jgen, SerializerProvider serializerProvider) throws IOException {
+        jgen.writeStartObject();
+        jgen.writeStringField("id", aClass.getId());
+        jgen.writeStringField("title", aClass.getTitle());
+        jgen.writeStringField("description", aClass.getDescription());
+        jgen.writeEndObject();
+    }
+
 }
